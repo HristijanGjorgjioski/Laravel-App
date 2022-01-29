0851de0e889b86b7a7e9f3b285ad289430d2a98a
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-01-29 19:49:23 +0100

    Created resource controller

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/about', [HomeController::class, 'about']);
-Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/about', [HomeController::class, 'about'])->name('home.about');
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
 
 Route::get('/store/{category?}/{item?}', function ($category = null, $item = null) {
     if (isset($category)) {
