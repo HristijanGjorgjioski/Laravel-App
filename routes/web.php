5a7d99c26906578b90470c96846da6b9655af004
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-01-29 10:22:53 +0100

    Creating Routes, Controllers and Views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [HomeController::class, 'about']);
+Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/store/{category?}/{item?}', function ($category = null, $item = null) {
+    if (isset($category)) {
+        if(isset($item)) {
+            return "You are viewing store for {$category} for {$item}";
+        }
+        return 'You are viewing store for ' . strip_tags($category);
+    }
+
+    return 'You are viewing all instruments';
 });
+
+// Route::get('/store', function () {
+//     $category = request('category');
+
+//     if (isset($category)) {
+//         return 'You are viewing store for ' . strip_tags($category);
+//     }
+
+//     return 'You are viewing all instruments';
+// });
